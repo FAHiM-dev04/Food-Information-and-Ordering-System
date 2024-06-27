5d594139d4f170b979f87e81fb2025da80abfabc
Md Fahim Shahriar <137883477+FAHiM-dev04@users.noreply.github.com>
2024-06-27 11:59:00 +0600

    Update project.c

--- project.c
+++ project.c
@@ -1,5 +1,5 @@
 /*
-Name of Project: Food Ordering System
+Name of Project: Food Information and Ordering System
 Functionalities: 1. Sign-up & Login
                  2. Validate user (to check password & other details of user, or if user already exists)
                  3. Search by hotel name
